68a55bbf618f0ff62599a875211b9fb3b1af4195
Victor Spichenkoff <victorspich13@gmail.com>
2024-01-02 11:33:27 -0300

    Fix head issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,15 @@ import '@/styles/components.css'
 import '@/styles/extraComponents.css'
 import '@/styles/pageNavigation.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head >
+    <title>Test Pagination</title>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
