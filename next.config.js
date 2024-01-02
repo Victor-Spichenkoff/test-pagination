c5af1c99f955483d64c9befb72dbaff2e1809ba2
Victor Spichenkoff <victorspich13@gmail.com>
2024-01-02 13:38:40 -0300

    Change next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+      path: '/out',
+    publicPath: '/test-pagination'
 }
 
 module.exports = nextConfig
