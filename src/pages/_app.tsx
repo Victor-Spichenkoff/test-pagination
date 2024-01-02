37afcfd66f2b0caa298a031d4bb67efc8fa962b4
Victor Spichenkoff <victorspich13@gmail.com>
2024-01-02 11:50:34 -0300

    Fix package.json issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,7 @@ import '@/styles/components.css'
 import '@/styles/extraComponents.css'
 import '@/styles/pageNavigation.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-    <Head >
-    <title>Test Pagination</title>
-    </Head>
-    <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      {/* <Head >
+      <Head >
         <title>Test Pagination</title>
-      </Head> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
