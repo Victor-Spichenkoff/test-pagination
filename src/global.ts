da6a6a63a60c923a7bb47d9c0b31c8ace0d177d4
Victor-Spichenkoff <victorspich13@gmail.com>
2024-07-15 12:41:44 -0300

    Adapted to new unsplash

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,4 @@
 export const code_key = '__chave_codigo'
 
 // export const baseUrl = 'http://localhost:2006'
-export const baseUrl = 'https://pagination-api-ugwo.onrender.com'
+export const baseUrl = process.env.NODE_ENV == "production" ? 'https://pagination-api-ugwo.onrender.com' : "http://localhost:2006" 
